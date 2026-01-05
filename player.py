075c18028844177c3b7e2dea9274af8091d5ba21
1transparentpng <aidens151@nycstudents.net>
2026-01-05 09:13:49 -0500

    inventory var added, effect and plyrturn class moved to progress branch

--- player.py
+++ player.py
@@ -11,31 +11,5 @@ def inc(stat, amt: int):
 atk = 0
 hp = 0
 
-
-
-
-class Effects:
-    def debuff(name: str, tier: int, time: int):
-        if name == "weaken":
-            dec(atk, (3*tier))
-        elif name == "poison":
-            dec(hp, (6*tier))
-        while time > 1:
-            time -= 1
-        if time == 0:
-            print(f"Effect {name} {tier} has ran out.")
-    
-    def buff(name: str, tier: int, time: int):
-        if name == "strength":
-            inc(atk, (3*tier))
-        elif name == "poison":
-            dec(hp, (6*tier))
-        while time > 1:
-            time -= 1
-        if time == 0:
-            print(f"Effect {name} {tier} has ran out.")
-
-
-class PlyrTurn:
-    def attack(atk, dmg: int):
+inventory = []
         
