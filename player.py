6e13f26886b8bfe0121af5704f41e8b75332e63d
1transparentpng <aidens151@nycstudents.net>
2026-01-05 09:11:38 -0500

    stat inc / dec, inventory var

---
+++ player.py
@@ -0,0 +1,20 @@
+def dec(stat, amt: int):
+    if stat == atk:
+        atk -= amt
+    elif stat == hp:
+         hp -= amt
+def inc(stat, amt: int):
+    if stat == atk:
+        atk += amt
+    elif stat == hp:
+         hp += amt
+## stats
+atk = 0
+hp = 0
+
+inventory = []
+
+
+
+
+        
