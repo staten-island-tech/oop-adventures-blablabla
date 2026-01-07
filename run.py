70f7ff82b18b47408581d18db55535c0b7fcbfa4
Rodneyz3 <ROdneyz3@nycstudents.net>
2026-01-07 09:26:17 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 class npc:
     
-    def __init_(self, name, dialouge, atk, hp):
+    def __init__(self, name, dialouge, atk, hp):
         self.__name=name
         self.__dialouge=dialouge
         self.__atk=atk
@@ -17,10 +17,10 @@ class npc:
     
     def status(self):
         print("**Npc's Stats**")
-        print(self.__name)
-        print(self.__dialouge)
-        print(self.__atk)
-        print(self.__hp)
+        print(f"Name:{self.__name}")
+        print(f"Dialouge:{self.__dialouge}")
+        print(f"Atk:{self.__atk}")
+        print(f"Hp:{self.__hp}")
     
 Npcbattle=[
     {"name":"Warlord","atk": 100, "hp": 100},
@@ -29,7 +29,20 @@ Npcbattle=[
     {"name": "Moon God", "atk": 129, "hp":72}    
 ]
 
-print(Npcbattle[1]["hp"])
+userchoice=int(input("choose a number from 0 to 3 to fight a random npc:"))
+print("you will be fighting", Npcbattle[userchoice]["name"])
+
+Npcfight=npc(Npcbattle[userchoice]["name"],"Let the fight begin", Npcbattle[userchoice]["atk"], Npcbattle[userchoice]["hp"])
+
+while True:
+    Npcfight.status()
+    break
+
+class boss(npc):
+    
+
+
+
 
             
 
