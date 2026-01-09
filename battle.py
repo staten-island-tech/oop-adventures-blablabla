55e50fa0a358cf388622bbcdeb84798b3c54faa3
1transparentpng <aidens151@nycstudents.net>
2026-01-09 09:26:28 -0500

    battle

---
+++ battle.py
@@ -0,0 +1,130 @@
+import random as ran
+
+
+
+class PlyrTurn:
+    def attack(atk): ## player will attack 
+        global npc_curhp
+        rand = ran.random(0.6, 1.5)
+        npc_curhp -= atk * rand
+        return rand
+    def ability(abil): ## player will use an ability 
+        if abil == "slateskin":
+            global playerDamageResist
+            global playerStunnedTurns
+            playerDamageResist += .2
+            playerStunnedTurns = 1
+        if abil == "sword":
+            global npc_curhp
+            global atk
+            rand = ran.random(0.6, 1.5)
+            npc_curhp -= (atk * rand)* 1.10
+            return rand 
+
+
+class NpcTurn:
+    def attack(atk):
+        global curhp
+        rand = ran.random(0.9, 1.3)
+        curhp -= atk * rand
+        print(f"the NPC attacked you and dealt {atk * rand} damage!")
+    def abil(abil):
+        global playerEffects
+        if abil == "poison_pwder":
+            playerEffects.append({
+                "name": "poison",
+                "turns": 2
+            })
+            print("the NPC threw a poisonous powder at you! You are now poisoned.")
+
+isBattle = False
+playersTurn = True
+playerDamageResist = 0
+playerDamageInflict = 0
+playerStunnedTurns = 0
+playerEffects = []
+
+while isBattle == True:
+    global curhp
+    global hp
+    global npc_curhp
+    global npc_hp
+    global atk
+    global npc_atk
+    global abil
+    global level
+    if playersTurn == True:
+        if playerStunnedTurns > 0:
+            playersTurn == False
+            continue
+        for effect in playerEffects:
+            if effect["name"] == "poison":
+                if effect["turns"] < 1:
+                    playerEffects.remove(effect)
+                    print("The poison inflicted upon you has worn off.")
+                else:
+                    curhp -= 6
+                    print("The poison inside your body drains your health.. ")
+                    effect["turns"] -= 1
+
+            if effect["name"] == "weaken":
+                if effect["turns"] < 1:
+                    playerEffects.remove(effect)
+                    print("The weakness you've been cursed with has withered away.")
+                else:
+                    atk = atk - 3
+                    print("A powerful effect weakens your strength..")
+                    effect["turns"] -= 1
+        if abil == "care":
+            curhp += 5 + level
+
+        
+        print("<ATTACK> | <ABILITY>")
+        option = input("What action would you like to do? >> ")
+        if option.lower() == "attack":
+            rand = PlyrTurn.attack(atk)
+            if rand > 1.5:
+                print(f"Critical strike! You dealt {atk * rand} damage.")
+            elif rand < .9:
+                print(f"Your strike was weak! You dealt {atk * rand} damage.")
+            else:
+                print(f"On spot! You dealt {atk * rand} damage.")
+        if option.lower() == "ability":
+            doAbil = PlyrTurn.ability(abil)
+            if abil == "sword":
+                print(f"Using your sharp sword, you managed to deal {(atk*rand)*1.10} damage!")
+                print("You spend the entirety of your next turn stashing your sword away though..")
+            if abil == "slateskin":
+                print("You drank a Slateskin Potion, granting resistance to the next attack against you!")
+                print("You are immobilized for the entirety of next turn as the effects wear off, though..")
+    else: ## NPC TURN
+        if curhp < hp / 2:
+            NpcTurn.attack(npc_atk)
+        elif npc_curhp > npc_hp / 2:
+            rand = ran.randint(1,3)
+            if rand != 3:
+                NpcTurn.attack(npc_atk)
+            else:
+                NpcTurn.attack("poison_pwder")
+
+            
+
+
+        
+
+
+            
+
+        
+
+
+
+
+
+            
+
+
+            
+            
+
+
